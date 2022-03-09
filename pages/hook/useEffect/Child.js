1bebbb53c5eb3335e2bdc7c890877391c4866702
王鹿白 <onechana@126.com>
2022-03-10 00:06:49 +0800

    useEffect

--- pages/hook/useEffect/Child.js
+++ pages/hook/useEffect/Child.js
@@ -1,5 +1,36 @@
-function Child(){
+import React, { useEffect, useState } from "react";
 
+function Child() {
+    console.log('Child()');
+
+    const [v, setv] = useState(0);
+    const [k, setk] = useState(0);
+
+    useEffect(() => {
+        console.log('init_1');
+        return () => {
+            console.log('clear_1');
+        }
+    })
+    useEffect(() => {
+        console.log('init_2');
+        return () => {
+            console.log('clear_2');
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log('init_3');
+        return () => {
+            console.log('clear_3');
+        }
+    }, [v])
+    return (
+        <>
+            <button onClick={() => { setv((old) => old + 1) }}>change v</button>
+            <button onClick={() => { setk((old) => old + 1) }}>change k</button>
+        </>
+    )
 }
 
 export default Child;
--- pages/hook/useEffect/index.js
+++ pages/hook/useEffect/index.js
@@ -7,3 +7,11 @@ ReactDOM.render(
     <Child></Child>,
     document.getElementById('root')
 )
+
+// 1:赋值给 useEffect 的函数会在组件渲染到屏幕之后执行
+
+// 2:effect 将在每轮渲染结束后___执行，但你可以选择让它 在只有___某些值改变的时候 才执行
+
+// 3:返回清除函数，组件卸载前/执行下一个effect前 执行
+
+// 4:useLayoutEffect 区别
